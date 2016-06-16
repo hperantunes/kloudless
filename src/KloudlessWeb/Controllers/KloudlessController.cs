06656fdf9cb59bf716deba9068e2dcceb6bc87cf
Hilton Perantunes <hperantunes@gmail.com>
2016-06-16 00:32:40 -0400

    Initial files

---
+++ src/KloudlessWeb/Controllers/KloudlessController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KloudlessWeb.Controllers
+{
+    [Route("api/kloudless")]
+    public class KloudlessController : Controller
+    {
+        [HttpGet("appid")]
+        public string Get()
+        {
+            // Kloudless AppId goes here!
+            return "";
+        }
+
+        [HttpPost("upload/{accountId:int}/{fileId:guid}")]
+        public void Upload(int accountId, Guid fileId)
+        {
+            
+        }
+    }
+}
---
+++ src/KloudlessWeb/wwwroot/js/site.js
@@ -0,0 +1,49 @@
+﻿$(function () {
+    var accountId,
+        service;
+
+    $(".external").click(function () {
+        if (accountId) {
+            saveFile();
+        }
+    });
+
+    $.get(
+        "/api/kloudless/appid"
+    )
+    .done(function (data) {
+        console.log("AppId: " + data);
+        Kloudless.authenticator(
+            $(".external"),
+            { 'app_id': data },
+            function (err, result) {
+                if (err) {
+                    $(".status").text("An error occurred! (" + err + ")");
+                    return;
+                }
+                accountId = result.id;
+                console.log("AccountId: " + accountId);
+
+                service = result.service;
+                Kloudless.stop($(".external"));
+                saveFile();
+            }
+        );
+    });
+
+    function saveFile() {
+        $(".status").text("Saving...");
+
+        var projectId = $('.project').val();
+        var folderId = $('.folder').val();
+        var fileId = $('.file').val();
+        var jqXHR = $.post(
+            "/api/kloudless/upload/" + accountId + "/" + fileId)
+            .done(function (data, status, jqXHR) {
+                $(".status").text("Done! (" + status + ")");
+            })
+            .fail(function () {
+                $(".status").text("An error occurred!");
+            });
+    }
+});
